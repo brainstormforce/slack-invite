c83d09a2cc692733c58a5b06dc7485dd7979afba
azher <azherk@bsf.io>
2016-01-14 15:31:48 +0530

    Footer link chaged

--- sia-admin/inc/views/base_footer.php
+++ sia-admin/inc/views/base_footer.php
@@ -15,7 +15,7 @@ BrainstormForce
     <!-- Footer -->
     <footer id="page-footer" class="content-mini content-mini-full font-s12 bg-gray-lighter clearfix">
         <div class="pull-right">
-            Crafted with <i class="fa fa-heart text-city"></i> by <a class="font-w600" href="http://goo.gl/vNS3I" target="_blank">Brainstorm Force</a>
+            Crafted with <i class="fa fa-heart text-city"></i> by <a class="font-w600" href="https://www.brainstormforce.com/" target="_blank">Brainstorm Force</a>
         </div>
         <div class="pull-left">
             <a class="font-w600" href="javascript:void(0)" target="_blank"><?php echo $one->name . ' ' . $one->version; ?></a> &copy; <span class="js-year-copy"></span>
