457dc772260724da40f1a693911b0d4756f49f33
azher <azherk@bsf.io>
2016-01-14 11:49:55 +0530

    sample file example created

---
+++ sia-config-sample.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The base configurations of the SlackInviteApplication.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * You can get the MySQL settings from your web host.
+ *
+ * This file is used by the sia-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "sia-config.php" and fill in the values.
+ *
+ * @package SlackInviteApplication
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for SlackInviteApplication */
+define('DB_NAME', 'database_name_here');
+
+/** MySQL database username */
+define('DB_USER', 'username_here');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'password_here');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
