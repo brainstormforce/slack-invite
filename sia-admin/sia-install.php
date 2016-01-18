b1873a75a0368030568db8162f9a347df6a5ad49
azher <azherk@bsf.io>
2016-01-18 11:12:49 +0530

    workin on slack changes

--- sia-admin/sia-install.php
+++ sia-admin/sia-install.php
@@ -52,17 +52,20 @@ if(isset($_POST['servername']) && !empty($_POST['servername']) && isset($_POST['
 	}else if(isset($_GET['conerr']) && !empty($_GET['conerr'])){
 	?>
 	<div class="add-data-basefull-width-error">
+		<div class="error_image">
+			<img src="assets/img/avatars/connecting-cables.jpg"/>
+		</div>
 		<h1>Error establishing a database connection</h1>
 		<hr></hr>
 		<p>This either means that the username and password information in your <code>sia-config.php</code> file is incorrect or we can't contact the database server at <code>localhost</code>. This could mean your host's database server is down.</p>
-		<ul>
+		<ul style="color:#646464;list-style: outside none none;margin-bottom: 33px;">
 			<li>Are you sure you have the correct username and password?</li>
 			<li>Are you sure that you have typed the correct hostname?</li>
 			<li>Are you sure that the database server is running?</li>
 		</ul>
 		<p>If you're unsure what these terms mean you should probably contact your host. If you still need help you can always visit the <a href="https://www.brainstormforce.com/">Brainstorm Force Support</a>.</p>
 		<p></p>
-		<p class="step"><a href="sia-install.php" class="button button-large">Try again</a></p>
+			<!--<p class="step"><a href="sia-install.php" class="button button-large">Try again</a></p>-->
 		
 	</div>
 	<?php
